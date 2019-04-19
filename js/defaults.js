e4eb6432d042f2d6dea1ab9f099d699b7f8458e8
ImpeachedPort <mcimpeachedport@gmail.com>
2019-04-18 20:39:29 -0400

    replaced sponsers with 2168's current sponsers

--- js/defaults.js
+++ js/defaults.js
@@ -4,14 +4,29 @@ defaults["eventkey"] = "2019miwmi";
 defaults["streamurl"] = "[auto]";
 defaults["showsponsers"] = 1;
 defaults["sponserpics"] = [
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/1.png",
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/2.png",
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/3.png",
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/4.png",
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/5.png",
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/6.png",
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/7.png",
-    "https://github.com/sykeben/BlueDashPro/raw/master/img/sponsers/8.png"
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/APS.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/COFCU.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/DavisStandard.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/DoDSTEM.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/ETM.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/GPS.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/Google.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/Groton%20Utilities.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/Hampden%20Engineering.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/Hillery.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/JSteele.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/L%2BM.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/MathWorks.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/MysticWoman_sClub.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/PCC%20STRUCTURALS.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/R-D.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/Solidworks.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/UBS_Logo.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/US_Air_Force_Logo_Solid_Colour.svg.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/Walmart_logo.svg.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/constellation-logo.png",
+    "https://raw.githubusercontent.com/russella02/2168SponsorLogos/master/pfizer.png"
+    
 ];
 
 function loadDefaults(loadsetup) {
