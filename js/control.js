ee734d981f6a0acc8dc25de21cf92701cf11266f
Nathan Kimpan <Nathankimpan@gmail.com>
2020-02-03 19:12:43 -0500

    I made some small changes

--- js/control.js
+++ js/control.js
@@ -1,7 +1,7 @@
 document.addEventListener('keydown', function(event) {
     var path = window.location.pathname;
     var page = path.split("/").pop();
-    if (event.keyCode == 37) { // 37 is the Key Code for The left         Key 
+    if (event.keyCode == 37 || event.keyCode == 188) { // 37 is the Key Code for The left Key || 188 is the Key Code for <
       
        if (page === "index.html"){
         window.location.replace("settings.html");
@@ -21,11 +21,21 @@ document.addEventListener('keydown', function(event) {
     }
     else if (event.keyCode == 83){ // 83 is the Key Code for S
         window.location.replace("settings.html");
+    }   
+    else if (event.keyCode == 49){ // 49 is the Key Code for 1
+        window.location.replace("index.html");
+    }
+    else if (event.keyCode == 50){ // 50 is the Key Code for 2
+        window.location.replace("lookahead.html");
+    }
+    else if (event.keyCode == 51){ // 51 is the Key Code for 3
+        window.location.replace("settings.html");
     } 
-    //else if (event.keyCode == 38) { // 38 is the Key Code for The up      Key 
+    //else if (event.keyCode == 38) { // 38 is the Key Code for The up Key 
        
     //}
-    else if (event.keyCode == 39) { // 39 is the Key Code for The right   Key 
+    else if (event.keyCode == 39 || event.keyCode == 190) { // 39 is the Key Code for The right Key || 190 is the Key Code for >
+    
         if (page === "index.html"){
             window.location.replace("lookahead.html");
            }
@@ -35,10 +45,11 @@ document.addEventListener('keydown', function(event) {
            else if (page === "settings.html"){
             window.location.replace("index.html");
            }
+    
 
     
     }
-    //else if (event.keyCode == 40) { // 40 is the Key Code for The down    Key 
+    //else if (event.keyCode == 40) { // 40 is the Key Code for The down Key 
   
    //}
 
